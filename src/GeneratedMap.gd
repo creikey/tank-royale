0a0f1e0246ba9045fc49ef2943eaa2a999a3b568
Cameron Reikes <cameronreikes@gmail.com>
2020-04-16 18:52:56 -0700

    Add basic maze generation and movement

---
+++ src/GeneratedMap.gd
@@ -0,0 +1,119 @@
+extends Node2D
+
+const map_size := 15
+
+export (PackedScene) var maze_cell_body_pack
+export (NodePath) var players_path
+
+#class Cell:
+#	var left_barrier := true
+#	var right_barrier := true
+#	var top_barrier := true
+#	var bottom_barrier := true
+#
+#	var visited = false
+const default_cell = {
+	"left_barrier": true,
+	"right_barrier": true,
+	"top_barrier": true,
+	"bottom_barrier": true
+}
+
+remote var id_to_spawnpoint := {}
+remote var cell_data: Array = []
+
+func _empty_map_array() -> Array:
+	var to_return: Array = []
+	to_return.resize(map_size)
+	for row in to_return.size():
+		to_return[row] = []
+		to_return[row].resize(map_size)
+		for column in to_return[row].size():
+			to_return[row][column] = default_cell.duplicate(true)
+	return to_return
+
+func generate_map():
+	randomize()
+
+	# construct the maze
+	cell_data = _empty_map_array()
+	
+	# carve paths
+	for row in cell_data.size():
+		for column in cell_data[row].size():
+			var is_carving_to_top: bool = bool(randi()%2)
+			if is_carving_to_top:
+				if row - 1 >= 0:
+					cell_data[row - 1][column]["bottom_barrier"] = false
+					cell_data[row][column]["top_barrier"] = false
+			else:
+				if column - 1 >= 0:
+					cell_data[row][column - 1]["right_barrier"] = false
+					cell_data[row][column]["left_barrier"] = false
+			if randi()%2 == 0 and row + 1 < cell_data.size():
+				cell_data[row + 1][column]["top_barrier"] = false
+				cell_data[row][column]["bottom_barrier"] = false
+
+	var maze_wall_length: float = maze_cell_body_pack.instance().wall_length # this is bad but idc
+	
+	var available_spawn_points = []
+	available_spawn_points.resize(map_size)
+	for row in available_spawn_points.size():
+		available_spawn_points[row] = []
+		available_spawn_points[row].resize(map_size)
+		for column in available_spawn_points[row].size():
+			available_spawn_points[row][column] = {"row":row, "column":column}
+	
+	# randomize row/column spawn points
+	for id in LobbySingleton.player_info.keys(): # because running on the server this includes everybody
+		# pick a spot in the available spawn points
+		var available_spawn_point_row: int = randi()%available_spawn_points.size()
+		var available_spawn_point_column: int = randi()%available_spawn_points[available_spawn_point_row].size()
+		
+		# get the row/column then set the spawnpoint
+		var spawn_point_data = available_spawn_points[available_spawn_point_row][available_spawn_point_column]
+		var row: int = spawn_point_data["row"]
+		var column: int = spawn_point_data["column"]
+		
+		print(id,"	",row,"	",column)
+		
+		id_to_spawnpoint[id] = Vector2(row, column)*maze_wall_length + Vector2(maze_wall_length, maze_wall_length)/2.0
+		
+		# remove the spawnpoint current player is spawning on
+		available_spawn_points[row].remove(column)
+		if available_spawn_points[row].size() <= 0:
+			available_spawn_points.remove(row)
+		
+#		id_to_spawnpoint[id] = Vector2(randi()%map_size, randi()%map_size)*maze_wall_length + Vector2(maze_wall_length, maze_wall_length)/2.0
+
+	rset("id_to_spawnpoint", id_to_spawnpoint)
+	rset("cell_data", cell_data)
+	rpc("add_maze_bodies")
+	rpc("spawn_players")
+
+func spawn_player(peer_id: int, info: Dictionary, is_my_player: bool, spawnpoint: Vector2):
+	var cur_player = preload("res://Player.tscn").instance()
+	cur_player.set_name(str(peer_id))
+	cur_player.get_node("Camera2D").current = is_my_player
+	cur_player.set_network_master(peer_id)
+	
+	cur_player.modulate = info["color"]
+	cur_player.username = info["username"]
+	
+	get_node(players_path).add_child(cur_player)
+	cur_player.position = spawnpoint
+
+remotesync func spawn_players():
+	var my_id: int = get_tree().get_network_unique_id()
+	if my_id != 1:
+		spawn_player(my_id, LobbySingleton.my_info, true, id_to_spawnpoint[my_id])
+	for id in LobbySingleton.player_info.keys():
+		spawn_player(id, LobbySingleton.player_info[id], false, id_to_spawnpoint[id])
+
+remotesync func add_maze_bodies():
+	for row in cell_data.size():
+		for column in cell_data[row].size():
+			var cur_maze_cell_body = maze_cell_body_pack.instance()
+			add_child(cur_maze_cell_body)
+			cur_maze_cell_body.position = Vector2(column, row)*cur_maze_cell_body.wall_length
+			cur_maze_cell_body.cell_data = cell_data[row][column]
---
+++ src/MazeCellBody.gd
@@ -0,0 +1,39 @@
+extends StaticBody2D
+
+const wall_length = 200.0
+const line_thickness = 8.0
+
+export var line_color = Color()
+
+var cell_data = null setget set_cell_data
+
+func set_cell_data(new_cell_data):
+	cell_data = new_cell_data
+	update()
+
+func _ready():
+	$LeftWall.shape.b.y = wall_length
+	$RightWall.position.x = wall_length
+	$TopWall.shape.b.x = wall_length
+	$BottomWall.position.y = wall_length
+	update()
+
+func _draw():
+	if cell_data == null:
+		return
+	if not has_node("LeftWall"):
+		return
+	for c in get_children():
+		c.disabled = true
+	if cell_data["top_barrier"]:
+		draw_line(Vector2(), Vector2(wall_length, 0), line_color, line_thickness)
+		$TopWall.disabled = false
+	if cell_data["bottom_barrier"]:
+		draw_line(Vector2(0, wall_length), Vector2(wall_length, wall_length), line_color, line_thickness)
+		$BottomWall.disabled = false
+	if cell_data["left_barrier"]:
+		draw_line(Vector2(), Vector2(0, wall_length), line_color, line_thickness)
+		$LeftWall.disabled = false
+	if cell_data["right_barrier"]:
+		draw_line(Vector2(wall_length, 0), Vector2(wall_length, wall_length), line_color, line_thickness)
+		$RightWall.disabled = false
